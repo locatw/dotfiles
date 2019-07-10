457a68f7809bb88e8a989c89832602646597c875
locatw <locatw@users.noreply.github.com>
2019-07-10 22:31:00 +0900

    add keybindings.json of VSCode

--- setup.bat
+++ setup.bat
@@ -3,7 +3,10 @@
 cd %~dp0
 
 set SETTINGS_JSON_PATH=%APPDATA%\Code\User\settings.json
+set KEYBINDINGS_JSON_PATH=%APPDATA%\Code\User\keybindings.json
+
 set BK_SETTINGS_JSON_PATH=%SETTINGS_JSON_PATH%.bk
+set BK_KEYBINDINGS_JSON_PATH=%KEYBINDINGS_JSON_PATH%.bk
 
 if exist "%SETTINGS_JSON_PATH%" (
     move %SETTINGS_JSON_PATH% %BK_SETTING_JSON_PATH%
@@ -11,3 +14,10 @@ if exist "%SETTINGS_JSON_PATH%" (
 )
 
 mklink %SETTINGS_JSON_PATH% %~dp0\vscode\settings.json
+
+if exist "%KEYBINDINGS_JSON_PATH%" (
+    move %KEYBINDINGS_JSON_PATH% %BK_KEYBINDINGS_JSON_PATH%
+    echo Back up %KEYBINDINGS_JSON_PATH% "->" %BK_KEYBINDINGS_JSON_PATH%
+)
+
+mklink %KEYBINDINGS_JSON_PATH% %~dp0vscode\keybindings.json
